862ef06d0074e859ffa9afe30f96f551ccfca19e
komalgumdel <komalgumdel09@gmail.com>
2025-08-18 18:42:42 +0530

    Add files via upload

---
+++ TASK1/script.js
@@ -0,0 +1,31 @@
+ var links=document.getElementById("links")
+        function showMenu(){
+            links.style.right="0";
+        }
+        function hideMenu(){
+            links.style.right="-200px";
+        }
+
+
+  const sections = document.querySelectorAll("section[id]");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - 100;
+      const sectionHeight = section.offsetHeight;
+      if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === `#${current}`) {
+        link.classList.add("active");
+      }
+    });
+  });
+
